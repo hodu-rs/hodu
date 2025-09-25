2a23997e052160e602102b59208b513669e3f986
Han Damin <miniex@daminstudio.net>
2025-09-25 16:35:11 +0900

    ci: add shell script for pulsih on cargo

---
+++ tools/cargo-publish.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+crates=(
+    hodu_core
+    hodu_nn/macros
+    hodu_nn
+
+    hodu_internal
+)
+
+if [ -n "$(git status --porcelain)" ]; then
+    echo "You have local changes!"
+    exit 1
+fi
+
+pushd crates
+
+for crate in "${crates[@]}"
+do
+  echo "Publishing ${crate}"
+  cp ../LICENSE "$crate"
+  pushd "$crate"
+  git add LICENSE
+  cargo publish --no-verify --allow-dirty
+  popd
+  sleep 20
+done
+
+popd
+
+echo "Publishing root crate"
+cargo publish --allow-dirty
+
+echo "Cleaning local state"
+git reset HEAD --hard
