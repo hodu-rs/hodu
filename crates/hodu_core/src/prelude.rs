4d9e3d77933d76c8db03ab4ca7e49f9b052e9b75
Han Damin <miniex@daminstudio.net>
2025-10-18 10:45:42 +0900

    feat: export HoduResult from core prelude

--- crates/hodu_core/src/prelude.rs
+++ crates/hodu_core/src/prelude.rs
@@ -4,6 +4,7 @@
 
 // Re-export core library types and functions
 pub use crate::backends::{builder::Builder, script::Script};
+pub use crate::error::HoduResult;
 pub use crate::scalar::Scalar;
 pub use crate::tensor::{set_runtime_device, Tensor};
 pub use crate::types::{backend::Backend, device::Device, dtype::DType};
